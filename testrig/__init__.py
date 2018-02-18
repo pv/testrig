4ea4711322851d04c43ee8da03f103c34cfd4ae4
Pauli Virtanen <pav@iki.fi>
2018-02-18 16:26:21 +0100

    Bump version to 0.2

--- testrig/__init__.py
+++ testrig/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 # This __version__ assignment is parsed by setup.py; keep it in this form.
 # Development versions end with ".dev" (suffix is added below).
-__version__ = "0.2.dev"
+__version__ = "0.2"
 __release__ = not __version__.endswith(".dev")
 
 try:
